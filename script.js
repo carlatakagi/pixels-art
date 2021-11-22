b5c5d22ccb39393ffb8398e1d3352b4122c1d9ff
carlatakagi <carlatakagi@gmail.com>
2021-11-22 14:47:40 -0400

    requisitos 2 e 3

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 // mudar cor de cada square
-const colorSquare1 = document.getElementsByClassName('color')[0];
-colorSquare1.style.backgroundColor = 'black';
+const square1 = document.getElementsByClassName('color')[0];
+square1.style.backgroundColor = 'black';
 
-const colorSquare2 = document.getElementsByClassName('color')[1];
-colorSquare2.style.backgroundColor = 'lightpurple';
+// mudar cor aleatória
+// consultei código de aleatoriedade de Cores
+// Link: https://dev.to/akhil_001/generating-random-color-with-single-line-of-js-code-fhj
+function randomColor() {
+  const randomColorSquare = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return randomColorSquare; // retornar cor aleatoria
+}
+const square2 = document.getElementsByClassName('color')[1];
+square2.style.backgroundColor = randomColor();
 
-const colorSquare3 = document.getElementsByClassName('color')[2];
-colorSquare3.style.backgroundColor = 'lightpink';
+const square3 = document.getElementsByClassName('color')[2];
+square3.style.backgroundColor = randomColor();
 
-const colorSquare4 = document.getElementsByClassName('color')[3];
-colorSquare4.style.backgroundColor = 'lightblue';
+const square4 = document.getElementsByClassName('color')[3];
+square4.style.backgroundColor = randomColor();
