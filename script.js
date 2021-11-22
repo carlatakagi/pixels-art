85e6c2318ea2613f1f6ea10431eb666524d61162
carlatakagi <carlatakagi@gmail.com>
2021-11-22 14:18:04 -0400

    requisito 2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // mudar cor de cada square
+const colorSquare1 = document.getElementsByClassName('color')[0];
+colorSquare1.style.backgroundColor = 'black';
+
+const colorSquare2 = document.getElementsByClassName('color')[1];
+colorSquare2.style.backgroundColor = 'lightpurple';
+
+const colorSquare3 = document.getElementsByClassName('color')[2];
+colorSquare3.style.backgroundColor = 'lightpink';
+
+const colorSquare4 = document.getElementsByClassName('color')[3];
+colorSquare4.style.backgroundColor = 'lightblue';
