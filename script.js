966c15f791fac4928a19f70df1b8fc23ae4c53f3
carlatakagi <carlatakagi@gmail.com>
2021-11-23 09:51:10 -0400

    arrumando requisitos 1 a 5

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // mudar cor do primeiro square
-const square1 = document.getElementsByClassName('color')[0];
+const square1 = document.getElementsByClassName('color-selected')[0];
 square1.style.backgroundColor = 'black';
 
 // mudar cor aleatória
@@ -15,16 +15,16 @@ square2.style.backgroundColor = randomColor();
 const square3 = document.getElementsByClassName('color')[2];
 square3.style.backgroundColor = randomColor();
 
-const square4 = document.getElementsByClassName('color')[3];
-square4.style.backgroundColor = randomColor();
+// const square4 = document.getElementsByClassName('color')[3];
+// square4.style.backgroundColor = randomColor();
 
 // selecionar cor
-const selectColor = document.getElementsById('color-palette');
+// const selectColor = document.getElementsById('color-palette');
 
 // botão limpar cores - requisito 9
 function clearButton() {
   const button = document.querySelector('#clear-board');
-  let backgroundSquare = randomColorSquare();
+  let backgroundSquare = randomColor();
   const newBackgroundColor = 'white';
 
   button.addEventListener('click', () => {
@@ -32,6 +32,7 @@ function clearButton() {
       if (button[index].style.backgroundSquare !== newBackgroundColor) {
         backgroundSquare = newBackgroundColor;
       }
+      return backgroundSquare;
     }
   });
 }
